5a49f2aa31776c4f6f492a5e221c208e2d127ffa
zoocasso <zoocasso1590@gmail.com>
2022-06-22 11:47:34 +0900

    220622_challenge2

---
+++ Challenge2-PixelArt/script.js
@@ -0,0 +1,60 @@
+/*
+ * Creates pixel art grid
+ * @param el DOM Element
+ * @param rows Number of rows
+ * @param rows Number of cols
+ */
+function PixelArt(el, rows, cols) {
+    const grid = document.querySelector(el);
+    for(let i1 = 0; i1 < cols; i1++){
+        const boxLine = document.createElement('div');
+        boxLine.setAttribute('class','boxLine');
+        for(j1 = 0; j1 < rows; j1++){
+            const squre = document.createElement('div');
+            squre.setAttribute('class','squre');
+            squre.setAttribute('draggable','true');
+            boxLine.appendChild(squre);
+        }
+        grid.appendChild(boxLine);
+    }
+    const boxLine = document.createElement('div');
+    boxLine.setAttribute('class','boxLine');
+    for(j1 = 0; j1 < rows; j1++){
+        const squre = document.createElement('div');
+        squre.setAttribute('class','squre');
+        squre.setAttribute('id','palette'+j1);
+        boxLine.appendChild(squre);
+    }
+    grid.appendChild(boxLine);
+    grid.addEventListener("click",onClick);
+    grid.addEventListener("drag",onDrag);
+    grid.addEventListener("dragstart",onDragstart);
+    grid.addEventListener("dragenter",onDragenter);
+
+    function onClick(event){
+        const target = event.target;
+        if(target.className == 'squre'){
+            target.style.backgroundColor = 'black';    
+        }
+    }
+    
+    function onDrag(event){
+        console.log('drag')
+    }
+    
+    function onDragstart(event){
+        const target = event.target;
+        if(target.className == 'squre'){
+            target.style.backgroundColor= 'gray';
+        }
+    }
+    
+    function onDragenter(event){
+        const target = event.target;
+        if(target.className == 'squre'){
+            target.style.backgroundColor= 'gray';
+        }
+    }
+}
+
+new PixelArt("#grid", 10, 10);

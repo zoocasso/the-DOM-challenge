b4bfc4eb6398c2f93830dfa32660f83cde2719b8
zoocasso <zoocasso1590@gmail.com>
2022-06-24 13:29:01 +0900

    Updated Challenge3

--- Challenge3-ChessBoard/script.js
+++ Challenge3-ChessBoard/script.js
@@ -1,15 +1,9 @@
-/*
- * Creates pixel art grid
- * @param el DOM Element
- * @param rows Number of rows
- * @param rows Number of cols
- */
-function PixelArt(el, rows, cols) {
+function PixelArt(el, side) {
     const grid = document.querySelector(el);
-    for(let i1 = 0; i1 < cols; i1++){
+    for(let i1 = 0; i1 < side; i1++){
         const boxLine = document.createElement('div');
         boxLine.setAttribute('class','boxLine');
-        for(let j1 = 0; j1 < rows; j1++){
+        for(let j1 = 0; j1 < side; j1++){
             const squre = document.createElement('div');
             squre.setAttribute('class','squre');
             squre.setAttribute('position',String(i1)+String(j1));
@@ -36,29 +30,44 @@ function PixelArt(el, rows, cols) {
         target.style.backgroundColor = 'red';
         const position = target.attributes.position.nodeValue.split('').map(x=>parseInt(x));
         let tempX,tempY,tempPosition,temp=0;
-        for(let i2 = 1; i2 < cols; i2++){
+        for(let i2 = 1; i2 < side; i2++){
             tempX = position[0]+i2;
             tempY = position[1]+i2;
-            tempPosition = [tempX, tempY].join('');    
+            tempPosition = [tempX, tempY].join('');
             temp = document.querySelector(`.squre[position="${tempPosition}"]`);
-            temp.style.backgroundColor = "red";
-            tempX = position[0]-i2;
-            tempY = position[1]-i2;
-            tempPosition = [tempX, tempY].join('');   
-            temp = document.querySelector(`.squre[position="${tempPosition}"]`);
-            temp.style.backgroundColor = 'red';
-            tempX = position[0]+i2;
-            tempY = position[1]-i2;
-            tempPosition = [tempX, tempY].join('');   
-            temp = document.querySelector(`.squre[position="${tempPosition}"]`);
-            temp.style.backgroundColor = 'red';
-            tempX = position[0]-i2;
-            tempY = position[1]+i2;
-            tempPosition = [tempX, tempY].join('');   
-            temp = document.querySelector(`.squre[position="${tempPosition}"]`);
-            temp.style.backgroundColor = 'red';
+            if(temp !== null){
+                temp.style.backgroundColor = "red";
+            }
         }
+        for(let i3 = 1; i3 < side; i3++){
+            tempX = position[0]-i3;
+            tempY = position[1]-i3;
+            tempPosition = [tempX, tempY].join('');
+            temp = document.querySelector(`.squre[position="${tempPosition}"]`);
+            if(temp !== null){
+                temp.style.backgroundColor = "red";
+            }
+        }
+        for(let i4 = 1; i4 < side; i4++){
+            tempX = position[0]+i4;
+            tempY = position[1]-i4;
+            tempPosition = [tempX, tempY].join('');
+            temp = document.querySelector(`.squre[position="${tempPosition}"]`);
+            if(temp !== null){
+                temp.style.backgroundColor = "red";
+            }
+        }
+        for(let i5 = 1; i5 < side; i5++){
+            tempX = position[0]-i5;
+            tempY = position[1]+i5;
+            tempPosition = [tempX, tempY].join('');
+            temp = document.querySelector(`.squre[position="${tempPosition}"]`);
+            if(temp !== null){
+                temp.style.backgroundColor = "red";
+            }
+        }            
     }
 }
 
-new PixelArt("#grid", 8, 8);
+
+new PixelArt("#grid", 8);
